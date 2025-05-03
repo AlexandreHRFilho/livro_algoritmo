3c75f9879a5b047145c7ccdfa1f8e06f7a9b8ee5
AlexandreHRFilho <alexandrehrfilho@gmail.com>
2025-05-02 21:38:05 -0300

    Dado uma lista qlq, devolver tudo em string

--- projeto01.py
+++ projeto01.py
@@ -1,2 +1,20 @@
-#teste alteracao 
-#Primeiro teste
+#Exercicio 1
+# dada uma lista , devolver em string
+def lista_para_string(lista):
+    if len(lista) ==0:
+        return ''
+    elif len(lista) ==1:
+        return lista[0]
+    else:
+        corpo = ', '.join(lista[:-1])
+        return corpo + ' e ' + lista[-1]
+#teste
+testes = [
+    [],
+    ['gato'],
+    ['gato', 'cachorro'],
+    ['gato', 'cachorro', 'rato']
+]
+
+for t in testes:
+    print(lista_para_string(t))
