1557f9af2302dd67ebbdc5087db82872897aae96
AlexandreHRFilho <alexandrehrfilho@gmail.com>
2025-05-04 03:13:12 -0300

    Sequencias de Collatz

---
+++ projeto03.py
@@ -0,0 +1,22 @@
+#sequencia de collatz
+#O problema de Collatz é um problema matemático que envolve uma sequência de números inteiros. A sequência começa com um número inteiro positivo n e segue as seguintes regras:
+#1. Se n é par, o próximo número na sequência é n / 2.
+#2. Se n é ímpar, o próximo número na sequência é 3n + 1.
+#3. A sequência termina quando n se torna 1.
+#A conjectura de Collatz afirma que, independentemente do número inicial n, a sequência sempre acabará chegando ao número 1. Embora tenha sido testada para muitos números, ainda não foi provada para todos os inteiros positivos.
+#A sequência de Collatz é um exemplo clássico de um problema que é fácil de entender, mas difícil de resolver matematicamente. Ela é frequentemente usada como um exemplo em teoria dos números e em estudos de algoritmos.
+
+def collatz(n):
+    if n <= 0:
+        raise ValueError("O número deve ser um inteiro positivo.")
+    sequencia = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+        sequencia.append(n)
+    return sequencia
+testes = [2, 3, 4, 10, 25, 40, 32, 100, 1000]
+for t in testes:
+    print(f"Sequência de Collatz para {t}: {collatz(t)}")
